9f46444e0b767e45415729fca60f68033d8d752b
islombek124 <8845414@mail.ru>
2023-02-13 15:40:21 +0400

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const buttons = document.querySelectorAll('.btn'),
   inp = document.getElementById('input'),
   clearBtn = document.querySelector('.clear__btn'),
-  equalBtn = document.querySelector('.there__btn'),
-  delBtn = document.querySelector('.del__btn');
+  equalBtn = document.querySelector('.there__btn');
 
 buttons.forEach((item) => {
   item.addEventListener('click', function(e){
@@ -14,7 +13,3 @@ equalBtn.addEventListener('click', () => {
   let result = eval(inp.value);
   inp.value = result;
 });
-
-// delBtn.addEventListener('click', ()=>{
-//   inp.value.splice(-1);
-// });
